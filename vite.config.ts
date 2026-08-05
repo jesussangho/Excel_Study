2646340c7ba8e491855a5a95ce70f63a123599e8
jesussangho <jesussangho@gmail.com>
2026-08-05 17:16:42 +0900

    fix: add base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Excel_Study/', // GitHub Pages 저장소 이름 추가
 })
